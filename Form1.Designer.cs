af585396a29dfc608b078e9633b4d3ad523e0de0
verelex <sdg7771@mail.ru>
2024-04-15 05:37:51 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace Oomnik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.labelRules = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1057, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(606, 298);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(200, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 93);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "L1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(350, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 93);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "L2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(500, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 93);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "L3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(650, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 93);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "L4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(800, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 93);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "L5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(950, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 93);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "L6";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(47, 399);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1147, 264);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "label8";
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // labelRules
+            // 
+            this.labelRules.AutoSize = true;
+            this.labelRules.Location = new System.Drawing.Point(29, 177);
+            this.labelRules.Name = "labelRules";
+            this.labelRules.Size = new System.Drawing.Size(83, 20);
+            this.labelRules.TabIndex = 11;
+            this.labelRules.Text = "labelRules";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1293, 506);
+            this.Controls.Add(this.labelRules);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Oomnik v.1 sum";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label labelRules;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oomnik
+{
+    public partial class Form1 : Form
+    {
+        private static int rnd1, rnd2, sum, t3iter;
+
+        private const String rules1 = "На экран последовательно выводятся ряды однозначных\n чисел по 6 в каждом, которые изображены в квадратах.\n Время экспозиции ряда - 3 секунды.\n Необходимо запомнить числа в том порядке, в котором\n они предъявлялись.\n По истечении 3-х секунд ряд чисел исчезает с экрана,\n а два квадрата подсвечиваются красным цветом.\n Необходимо вспомнить ряд чисел и сложить в уме два\n числа, которые отображались в помеченных квадратах.\n При помощи клавиатуры записать получившуюся сумму в\n поле ввода и нажать ENTER.\n На обдумывание и запись отводится 30 секунд. Если\n запись за это время не состоялась, то на экране\n появится следующий ряд чисел, а предыдущее задание\n считается невыполненным.\n  ";
+
+        private static LinkedList<Label> listLabels;
+
+        //Function to get random number
+        private static readonly Random getrandom = new Random();
+
+        public static int GetRandomNumber(int min, int max) // включительно
+        {
+            lock (getrandom) // synchronize
+            {
+                return getrandom.Next(min, max + 1);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer2.Tick += new EventHandler(timer2_Tick);
+            timer2.Tick += new EventHandler(timer3_Tick);
+            timer1.Interval = 3000; // интервал показа 6 чисел
+            timer2.Interval = 30000; // интервал обдумывания и ввода суммы чисел
+            timer3.Interval = 1000; // интервал отображения 30 сек --
+            timer1.Enabled = false; // выключены
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listLabels = new LinkedList<Label>();
+
+            listLabels.AddLast(label1);
+            listLabels.AddLast(label2);
+            listLabels.AddLast(label3);
+            listLabels.AddLast(label4);
+            listLabels.AddLast(label5);
+            listLabels.AddLast(label6);
+
+            int i = 1;
+
+            for (LinkedListNode<Label> rn = listLabels.First; rn != null; rn = rn.Next)
+            {
+                var Val = rn.Value;
+                Val.Name = "L" + i;
+                rn.Value = Val;
+                i++;
+            }
+
+            label7.Text = String.Empty; // очищаем вывод результатов
+            label8.Text = String.Empty; // очищаем вывод отсчета 30 сек
+
+            //----------------------------------------------------------------------------------
+
+            // Set up the delays for the ToolTip.
+            toolTip1.AutoPopDelay = 30000;
+            toolTip1.InitialDelay = 1000;
+            toolTip1.ReshowDelay = 100;
+            // Force the ToolTip text to be displayed whether or not the form is active.
+            toolTip1.ShowAlways = true;
+
+            // Set up the ToolTip text for the Button and Checkbox.
+            toolTip1.SetToolTip(this.button1, rules1);
+
+            labelRules.Text = rules1;
+        }
+
+        private void GenData() // основной раунд
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+
+            foreach (var v in listLabels)
+            {
+                int x = GetRandomNumber(0, 9);
+                v.Text = x.ToString();
+                v.BackColor = Color.White; // цвет фона белый у всех квадратов
+                v.ForeColor = Color.Black; // цвет шрифта черный у всех квадратов
+            }
+
+            // получаем два случайных значения от 0 до 5
+            rnd1 = GetRandomNumber(0, 5);
+            int x1 = Int32.Parse(listLabels.ElementAt(rnd1).Text);
+
+            do
+            {
+                rnd2 = GetRandomNumber(0, 5);
+            }
+            while (rnd1 == rnd2);
+
+            int x2 = Int32.Parse(listLabels.ElementAt(rnd2).Text);
+
+            sum = x1 + x2; // сумма чисел в двух случайных квадратах
+
+            timer1.Enabled = true; // начало ожидания 3 сек
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false; // скрываем кнопку запуска
+            GenData();
+            labelRules.Visible = false;
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox1.Text.Equals(sum.ToString()))
+                {
+                    label7.Text += "1"; // введен правильный ответ
+                }
+                else
+                {
+                    label7.Text += "0"; // введен неправильный ответ
+                }
+                timer2.Enabled = false; // остан. 30 сек интервал т.к. введено значение раньше срабатывания таймера
+                timer3.Enabled = false;
+                textBox1.Text = String.Empty; // очистка поля ввода
+                GenData();
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;  // чтобы не проигрывался звук предупреждения в винде
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolTip1.BackColor = Color.Yellow;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // 3 секунды истекло, запуск 30 сек
+        {
+            timer1.Enabled = false; // останавливаем 1 таймер
+
+            foreach (var v in listLabels)
+            {
+                v.ForeColor = Color.White; // шрифт белый, чтобы скрыть
+            }
+            listLabels.ElementAt(rnd1).BackColor = Color.Red; // подсвечиваем красным рандомные квадраты
+            listLabels.ElementAt(rnd1).ForeColor = Color.Red;
+            listLabels.ElementAt(rnd2).BackColor = Color.Red;
+            listLabels.ElementAt(rnd2).ForeColor = Color.Red;
+
+            timer2.Enabled = true; // отсчет 30 секунд
+            t3iter = 30;
+            timer3.Enabled = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) // 30 секунд истекло
+        {
+
+            if (timer2.Enabled == true) // почему-то он иногда срабатывает 2 раза
+            {
+                timer2.Enabled = false; // останавливаем 2 таймер
+                timer3.Enabled = false; // останавливаем 3 таймер
+                label7.Text += "0"; // invalid answer
+                GenData();
+            }
+            
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (timer2.Enabled == true)
+            {
+                label8.Text = t3iter.ToString();
+                t3iter--;
+            }
+        }
+
+    }
+}
